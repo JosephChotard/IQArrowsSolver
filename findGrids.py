5240ef368f22eb8b9e03f05d61eff586bfb1c38a
JosephChotard <josephchotard1@gmail.com>
2020-12-27 11:08:38 +0100

    take into account constraints when finding grids

--- findGrids.py
+++ findGrids.py
@@ -4,8 +4,22 @@ from board import Board
 
 pieces = set(colour for colour in PIECES.keys())
 
+CONSTRAINTS = {
+  (0,1): (2, ''),
+  (1,0): (2, ''),
+  (1,3): (2, ''),
+  (2,4): (2, ''),
+}
+
 with open('grids.json') as json_file:
     data = json.load(json_file)
     for grid in data:
       board = Board(grid=grid, placedPieces=pieces)
-      print(board)
+      correct = True
+      for constraint in CONSTRAINTS.keys():
+        if board.grid[constraint[0]][constraint[1]][0] != CONSTRAINTS[constraint][0]:
+          correct = False
+          continue
+      if correct:
+        print(board)
+        print()
