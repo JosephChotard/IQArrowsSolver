5a96ba63f5e98ac24e9885e7d23d5fdddea0e5e8
JosephChotard <josephchotard1@gmail.com>
2020-12-29 01:29:59 +0100

    link to solution

--- website/pages/problems/index.tsx
+++ website/pages/problems/index.tsx
@@ -6,6 +6,7 @@ import Router from 'next/router'
 import constraintToQuery from 'lib/constraintToQuery'
 import windowLocationToConstraint from 'lib/windowLocationToConstraint'
 import styles from './Problems.module.scss'
+import Link from 'next/link'
 
 const WIDTH = 6
 const HEIGHT = 3
@@ -13,15 +14,15 @@ const HEIGHT = 3
 export default function Problems() {
   const [grid, setGrid] = React.useState<BOARD>(null)
 
-  const generateNewPuzzle = () => {
+  const generateNewPuzzle = (replace=false) => {
     setGrid(null)
     const g = new Array(HEIGHT).fill(DIRECTIONS.NONE).map(() => new Array(WIDTH).fill([DIRECTIONS.NONE, '']))
     const givenCells = Math.floor(Math.random() * (5 - 3 + 1)) + 3;
     fetch(`${process.env.basePath ? process.env.basePath : ''}/constraints/constraints-${givenCells}.txt`)
       .then(data => data.text())
-      .then(constraintsText => { 
+      .then(constraintsText => {
         const constraints = constraintsText.split(/\r\n|\r|\n/)
-        const selectedConstraintIndex = Math.floor(Math.random() * (constraints.length - 1 + 1));
+        const selectedConstraintIndex = Math.floor(Math.random() * (constraints.length - 1 + 1))
         const selectedConstraint = constraints[selectedConstraintIndex]
         const parsedConstraintArray = selectedConstraint
           .substring(1, selectedConstraint.length - 2)
@@ -30,7 +31,7 @@ export default function Problems() {
             .substring(1)
             .split('): (')
             .map(pair => pair.split(', '))
-        )
+          )
         let parsedConstraint = {}
         parsedConstraintArray.forEach(constraint => {
           parsedConstraint[`${constraint[0][0]}c${constraint[0][1]}`] = constraint[1][0]
@@ -39,10 +40,16 @@ export default function Problems() {
 
         // Set the url
         const href = `/problems/${constraintToQuery(parsedConstraint)}`
-        Router.replace(href, href, {
+        if (replace) {
+          Router.replace(href, href, {
             shallow: true,
           })
-        })
+        } else {
+          Router.push(href, href, {
+            shallow: true,
+          })
+        }
+      })
   }
 
   React.useEffect(() => {
@@ -53,12 +60,17 @@ export default function Problems() {
       setGrid(applyConstraintToGrid(g, params))
       return
     }
-    generateNewPuzzle()
+    generateNewPuzzle(true)
   }, [])
 
   return (
     <div className={styles.problems}>
       <h1>Here is a brand new puzzle for you to solve</h1>
+      <p>As always, there is only one possible solution. You can find it 
+       <Link href={`/${window.location.search}`}>
+          <a className={styles.solutionLink}>here</a>
+        </Link>
+      </p>
       <div>
         {grid === null
           ? <div className={`${styles.loading} ${styles.board}`}>
@@ -74,7 +86,7 @@ export default function Problems() {
         }
       </div>
       <div className={styles.generateNewPuzzle}>
-        <button onClick={generateNewPuzzle} className={styles.generateNewPuzzleButton}>
+        <button onClick={() => generateNewPuzzle()} className={styles.generateNewPuzzleButton}>
           Generate puzzle
         </button>
       </div>
